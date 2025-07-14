c6db690e8e0196df4a642b8008f953f3a823f4b2
GJZ26 <adolfogj26@gmail.com>
2025-07-13 23:34:21 -0600

    fix: cors

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,25 @@
-import dotenv from 'dotenv'
-import express from 'express'
-import cors from 'cors'
-import router from './medication/infrastructure/router'
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import router from "./medication/infrastructure/router";
 
-dotenv.config()
+dotenv.config();
 
-const APP_PORT = process.env.PORT || 3000
-const server = express()
+const APP_PORT = process.env.PORT || 3000;
+const server = express();
 
-server.use(cors())
-server.use(express.json())
+server.use(
+  cors({
+    origin: ["*"],
+    methods: ["*"],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
+server.use(express.json());
 
-server.use('/medication', router)
+server.use("/medication", router);
 
 server.listen(APP_PORT, () => {
-    console.log(`Server running on port ${APP_PORT}`)
-})
+  console.log(`Server running on port ${APP_PORT}`);
+});
